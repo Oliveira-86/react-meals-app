9339a07f587b072114f015cf10eab027860c014f
oliveira-86 <egberto.duarte.14@gmail.com>
2021-11-15 20:04:38 -0300

    Fetching Meals Data with Firebas

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealsItem from './MealsItem/MealsItem';
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+import axios from 'axios';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const AvailableMeals = () => {
+    const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [color] = useState('#ffffff');
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                const response = await axios.get(
+                    'https://food-order-app-react-38ac4-default-rtdb.firebaseio.com/meals'
+                );
+                const resData = response.data;
+
+                const loadMeals = [];
+
+                for (const key in resData) {
+                    loadMeals.push({
+                        id: key,
+                        name: resData[key].name,
+                        description: resData[key].description,
+                        price: resData[key].price,
+                    });
+                }
+
+                setMeals(loadMeals);
+                setLoading(false);
+            } catch (error) {
+                if(error.request) {
+                    setError(error.message);
+                    setLoading(false);
+                }
+            }
+        };
+
+        fetchMeals();
+    }, []);
+
+    if (loading) {
+        return (
+            <section className={classes.LoadingContainer}>
+                <ClipLoader color={color} loading={loading} size={100} />
+            </section>
+        );
+    }
+
+    if (error) {
+        return (
+            <section className={classes.LoadingContainer}>
+                <p>{`${error} try again!`}</p>
+            </section>
+        );
+    }
+
     return (
         <section className={classes.meals}>
             <Card>
                 <ul>
-                    {DUMMY_MEALS.map((meal) => (
+                    {meals.map((meal) => (
                         <MealsItem
                             key={meal.id}
                             price={meal.price}
